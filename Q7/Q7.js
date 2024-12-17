f8d4388137be8fd479c672587b6f447e5eaea0d9
Joao-Vitor69 <jvab1@aluno.ifal.edu.br>
2024-12-16 22:28:55 -0300

    add questoes

---
+++ Q7/Q7.js
@@ -0,0 +1,10 @@
+let palavra = prompt("Digite sua palavra: ")
+
+let palavrainve = ""
+
+for (let i = 0; i < palavra.length; i++) {
+
+    palavrainve = palavra[i] + palavrainve
+    
+}
+alert(palavrainve)
